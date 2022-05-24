2bdfc8ecaa576b935cc7ea3a9ad9ae2a8065c7d8
Ahmed EL-Gammal <ahmedmabdalla91@gmail.com>
2022-05-24 16:24:56 +0200

    upgrated desgin

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -1,16 +1,3 @@
-
-<!--
-Template Name: Metronic - Bootstrap 4 HTML, React, Angular 9 & VueJS Admin Dashboard Theme
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: https://1.envato.market/EA4JP
-Renew Support: https://1.envato.market/EA4JP
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
 <html lang="en" >
     <!--begin::Head-->
     <head>
@@ -25,27 +12,27 @@ License: You must have a valid license purchased only from themeforest(the above
         <!--end::Fonts-->
 
         <!--begin::Page Vendors Styles(used by this page)-->
-            <link href="{{asset('metronic/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
         <!--end::Page Vendors Styles-->
 
         <!--begin::Page Vendors Styles(used by this page)-->
-        <link href="{{asset('metronic/assets/plugins/custom/datatables/datatables.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ asset('metronic/assets/plugins/custom/datatables/datatables.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
         <!--end::Page Vendors Styles-->
 
         <!--begin::Global Theme Styles(used by all pages)-->
-            <link href="{{asset('metronic/assets/plugins/global/plugins.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
-            <link href="{{asset('metronic/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
-            <link href="{{asset('metronic/assets/css/style.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/plugins/global/plugins.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/css/style.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
         <!--end::Global Theme Styles-->
 
         <!--begin::Layout Themes(used by all pages)-->
-            <link href="{{asset('metronic/assets/css/themes/layout/header/base/light.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
-            <link href="{{asset('metronic/assets/css/themes/layout/header/menu/light.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
-            <link href="{{asset('metronic/assets/css/themes/layout/brand/dark.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
-            <link href="{{asset('metronic/assets/css/themes/layout/aside/dark.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/css/themes/layout/header/base/light.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/css/themes/layout/header/menu/light.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/css/themes/layout/brand/dark.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+            <link href="{{ asset('metronic/assets/css/themes/layout/aside/dark.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
         <!--end::Layout Themes-->
 
-        <link rel="shortcut icon" href="{{asset('img/4go-white.png')}}"/>
+        <link rel="shortcut icon" href="{{ asset('img/4go-white.png') }}"/>
 
     </head>
     <!--end::Head-->
@@ -290,65 +277,68 @@ License: You must have a valid license purchased only from themeforest(the above
 
         <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
         <!--begin::Global Config(global config for global JS scripts)-->
+        
         <script>
+
             var KTAppSettings = {
-                    "breakpoints": {
-                        "sm": 576,
-                        "md": 768,
-                        "lg": 992,
-                        "xl": 1200,
-                        "xxl": 1400
-                    },
-                    "colors": {
-                        "theme": {
-                            "base": {
-                                "white": "#ffffff",
-                                "primary": "#3699FF",
-                                "secondary": "#E5EAEE",
-                                "success": "#1BC5BD",
-                                "info": "#8950FC",
-                                "warning": "#FFA800",
-                                "danger": "#F64E60",
-                                "light": "#E4E6EF",
-                                "dark": "#181C32"
-                            },
-                            "light": {
-                                "white": "#ffffff",
-                                "primary": "#E1F0FF",
-                                "secondary": "#EBEDF3",
-                                "success": "#C9F7F5",
-                                "info": "#EEE5FF",
-                                "warning": "#FFF4DE",
-                                "danger": "#FFE2E5",
-                                "light": "#F3F6F9",
-                                "dark": "#D6D6E0"
-                            },
-                            "inverse": {
-                                "white": "#ffffff",
-                                "primary": "#ffffff",
-                                "secondary": "#3F4254",
-                                "success": "#ffffff",
-                                "info": "#ffffff",
-                                "warning": "#ffffff",
-                                "danger": "#ffffff",
-                                "light": "#464E5F",
-                                "dark": "#ffffff"
-                            }
+                "breakpoints": {
+                    "sm": 576,
+                    "md": 768,
+                    "lg": 992,
+                    "xl": 1200,
+                    "xxl": 1400
+                },
+                "colors": {
+                    "theme": {
+                        "base": {
+                            "white": "#ffffff",
+                            "primary": "#3699FF",
+                            "secondary": "#E5EAEE",
+                            "success": "#1BC5BD",
+                            "info": "#8950FC",
+                            "warning": "#FFA800",
+                            "danger": "#F64E60",
+                            "light": "#E4E6EF",
+                            "dark": "#181C32"
                         },
-                        "gray": {
-                            "gray-100": "#F3F6F9",
-                            "gray-200": "#EBEDF3",
-                            "gray-300": "#E4E6EF",
-                            "gray-400": "#D1D3E0",
-                            "gray-500": "#B5B5C3",
-                            "gray-600": "#7E8299",
-                            "gray-700": "#5E6278",
-                            "gray-800": "#3F4254",
-                            "gray-900": "#181C32"
+                        "light": {
+                            "white": "#ffffff",
+                            "primary": "#E1F0FF",
+                            "secondary": "#EBEDF3",
+                            "success": "#C9F7F5",
+                            "info": "#EEE5FF",
+                            "warning": "#FFF4DE",
+                            "danger": "#FFE2E5",
+                            "light": "#F3F6F9",
+                            "dark": "#D6D6E0"
+                        },
+                        "inverse": {
+                            "white": "#ffffff",
+                            "primary": "#ffffff",
+                            "secondary": "#3F4254",
+                            "success": "#ffffff",
+                            "info": "#ffffff",
+                            "warning": "#ffffff",
+                            "danger": "#ffffff",
+                            "light": "#464E5F",
+                            "dark": "#ffffff"
                         }
                     },
-                    "font-family": "Poppins"
-                };
+                    "gray": {
+                        "gray-100": "#F3F6F9",
+                        "gray-200": "#EBEDF3",
+                        "gray-300": "#E4E6EF",
+                        "gray-400": "#D1D3E0",
+                        "gray-500": "#B5B5C3",
+                        "gray-600": "#7E8299",
+                        "gray-700": "#5E6278",
+                        "gray-800": "#3F4254",
+                        "gray-900": "#181C32"
+                    }
+                },
+                "font-family": "Poppins"
+            };
+
         </script>
         <!--end::Global Config-->
 
@@ -388,6 +378,7 @@ License: You must have a valid license purchased only from themeforest(the above
             });
 
         </script>
+
         @yield('scripts')
 </body>
     <!--end::Body-->
